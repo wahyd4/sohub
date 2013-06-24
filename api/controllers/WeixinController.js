07da4e3fab8d8c9417c81d8cfd2f7ee6f7e108d9
junv <wahyd4@gmail.com>
2013-06-24 22:26:35 +0800

    add model message

--- api/controllers/WeixinController.js
+++ api/controllers/WeixinController.js
@@ -2,16 +2,24 @@ var wechat = require('wechat');
 var WeixinController = {
 
     auth: wechat('filhsafghJOj323kskdv', function (req, res, next) {
-        // 微信输入信息都在req.weixin上
-        var message = req.weixin;
-        res.reply('hehe');
+        res.reply(' ');
     }),
 
 
     query: wechat('filhsafghJOj323kskdv', function (req, res, next) {
         var message = req.weixin;
-        console.log('message======' + message)
-        res.reply('hehe');
+        console.log(message);
+        Message.create({
+            content: message.Content,
+            createTime: message.CreateTime,
+            fromUser: message.FromUserName,
+            toUser: message.ToUserName,
+            messageType: message.MsgType,
+            messageId: message.MsgId
+        }).done(function (err, message) {
+                res.reply('你到消息：' + message.content + '已经成功收到！！');
+            });
+//        res.reply('Hello, this is demo');
     })
 
 };
---
+++ api/models/Message.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+    attributes: {
+
+        content: 'STRING',
+        createTime: 'FLOAT',
+        toUser: 'STRING',
+        fromUser: 'STRING',
+        messageType: 'STRING',
+        messageId: 'FLOAT'
+
+    }
+
+};
