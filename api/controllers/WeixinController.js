f4296abdbb509dde21069efc946fa794422d4036
junv <wahyd4@gmail.com>
2013-06-24 21:55:19 +0800

    try to make weixin node client works

--- api/controllers/WeixinController.js
+++ api/controllers/WeixinController.js
@@ -1,17 +1,13 @@
 var wechat = require('wechat');
 var WeixinController = {
 
-    auth: function (req, res) {
-        wechat('filhsafghJOj323kskdv', function (req, res, next) {
-            // 微信输入信息都在req.weixin上
-            var message = req.weixin;
-            console.log('message======' + message)
-            res.reply('hehe');
-        })();
+    auth: wechat('filhsafghJOj323kskdv', function (req, res, next) {
+        // 微信输入信息都在req.weixin上
+        var message = req.weixin;
+        console.log('message======' + message)
+        res.reply('hehe');
+    }),
 
-
-
-    },
 
     query: function (req, res) {
 
