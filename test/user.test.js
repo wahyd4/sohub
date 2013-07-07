bb44f81ae680df80d4588828154e7054f6d32651
junv <wahyd4@gmail.com>
2013-07-07 22:54:37 +0800

    add set display name

---
+++ test/user.test.js
@@ -0,0 +1,20 @@
+var User = require('../api/models/User.js');
+
+var assert = require('assert');
+
+describe('test user model', function () {
+
+//    var userInstance = null;
+//    before(function () {
+//        User.create({
+//            name: 'oldName'
+//        }).done(function (err, user) {
+//                userInstance = user;
+//            });
+//    });
+//
+//    it('the user instance should not be null', function () {
+//        assert.equals(userInstance === null, false);
+//    });
+
+});
